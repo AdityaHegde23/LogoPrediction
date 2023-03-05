7767710fbf5179c0985de246bf91eb4b0b2e2c39
AD <adityahegde226@gmail.com>
2023-03-05 12:53:17 +0530

    testing get and post

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
 """Modules"""
+import uvicorn
 from fastapi import FastAPI
 from . import schema
 
 app = FastAPI()
 
+@app.get('/')
+def index():
+    """ Initial root"""
+    return {'message': 'Welcome to Logo Prediction app'}
 
 @app.post("/items")
-async def root(item: schema.Item):
-    """ Initial root"""
-    return {item}
+async def root(item: str):
+    return {'message':f'String is {item}'}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+
+
+"""
+uvicorn main:app --reload
+"""
